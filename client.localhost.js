be7bacb963e2c7e660bb068d3018f3943b6a25d2
SomeKittens <rkoutnik@gmail.com>
2013-04-03 14:34:03 -0400

    Redid UI with help from @rlemon

--- client.localhost.js
+++ client.localhost.js
@@ -9,6 +9,8 @@ $(function () {
     , addMessage = function(author, message, color, dt) {
       content.append('<p><span style="color:' + color + '">' + author + '</span> @ ' + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':' + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes()) + ': ' + message + '</p>');
     };
+    
+  //When the user picks a username, emit a login event
 
   var connection = io.connect('http://localhost/');
   
@@ -26,11 +28,7 @@ $(function () {
   connection.on('message', function (message) {
     console.log(message);
     //TODO: Make this whole system make sense with custom events
-    if (message.type === 'color') { 
-      myColor = message.data;
-      status.text(myName + ': ').css('color', myColor);
-      input.removeAttr('disabled').focus();
-    } else if (message.type === 'history') { 
+    if (message.type === 'history') { 
       for (var i = 0; i < message.data.length; i++) {
         addMessage(message.data[i].author, message.data[i].text, message.data[i].color, new Date(message.data[i].time));
       }
@@ -42,23 +40,39 @@ $(function () {
     }
   });
   
+  connection.on('newUser', function(message) {
+    myColor = message.color;
+    status.text(myName + ': ').css('color', myColor);
+    input.removeAttr('disabled').focus();
+  });
+  
+  connection.on('connect_failed', function() {
+    status.text('Error');
+    input.attr('disabled', 'disabled').val('Unable to comminucate with the WebSocket server.');
+  });
+  
+  //Set up our Bootstrap stuff
+  $('#selectNameModal').modal('show');
+  $('#selectNameModal').on('hide', function() {
+    myName = $('#username_select').val();
+    connection.emit('login', {username: myName});
+  });
+  $('#page-info .close').on('click', function() {
+    $(this).parent().remove();
+  });
+  
   input.keydown(function (e) {
     if (e.keyCode === 13) {
       var msg = $(this).val();
       if (!msg) {
         return;
       }
-      connection.send(msg);
+      connection.emit('message', msg);
       $(this).val('');
       input.attr('disabled', 'disabled');
       if (myName === false) {
         myName = msg;
       }
     }
-  });
-  
-  connection.on('connect_failed', function() {
-    status.text('Error');
-    input.attr('disabled', 'disabled').val('Unable to comminucate with the WebSocket server.');
   });
 });
